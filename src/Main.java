652beb74b9507b1365ad85363d28605e3fdeb517
AndresD <felipediazc08@gmail.com>
2026-02-26 14:04:03 -0500

    IMC

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner teclado=new Scanner (System.in);
+        double peso;
+        double estatura;
+        double imc;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Escriba su peso en kilogramos");
+        peso=teclado.nextDouble();
+        System.out.println("Escriba su estatura ");
+        estatura=teclado.nextDouble();
+        imc=peso/estatura;
+        if(imc<=18.5){
+            System.out.println("su situacion es un pejo bajo");
+        }else if(imc<=18.5 && imc>=24.9){
+            System.out.println("su situacion es de peso normal");
+        }else if(imc >=25 && imc <=25.9){
+            System.out.println("su situacion es sobre peso de grado 1");
+        }else if(imc >= 27 && imc <=29.9){
+            System.out.println("su situacion es sobre peso de grado 2");
+        }else if(imc >= 30 && imc <=34.9){
+            System.out.println("su situacion es obecidad de tipo 1");
+        }else if(imc >= 35 && imc <=39.9){
+            System.out.println("su situacion es obecidad de tipo 2");
+        }else if(imc >= 40 && imc <=49.9){
+            System.out.println("su situacion es obecidad extrema");
+        }else{
+
         }
+        System.out.println("su indice de masa corporal es (IMC)"+imc);
     }
 }
